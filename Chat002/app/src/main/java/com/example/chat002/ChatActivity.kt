afef025b8975ab2420f77d5f9665c58502509fad
YS KIM <mscyskim032@gmail.com>
2025-02-08 15:54:15 +0900

    기본기능 완성 파일

--- Chat002/app/src/main/java/com/example/chat002/ChatActivity.kt
+++ Chat002/app/src/main/java/com/example/chat002/ChatActivity.kt
@@ -11,13 +11,89 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
+//class ChatActivity : AppCompatActivity() {
+//
+//    private lateinit var receiverName: String
+//    private lateinit var receiverUid: String
+//
+//    private lateinit var binding: ActivityChatBinding
+//
+//
+//    lateinit var mAuth: FirebaseAuth
+//    lateinit var mDbRef: DatabaseReference
+//
+//    private lateinit var receiverRoom: String
+//    private lateinit var senderRoom: String
+//
+//    private lateinit var messageList:ArrayList<Message>
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding= ActivityChatBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        messageList = ArrayList()
+//        val messageAdapter : MessageAdapter = MessageAdapter(this, messageList)
+//
+//        binding.chatRecyclerView.layoutManager=LinearLayoutManager(this)
+//        binding.chatRecyclerView.adapter = messageAdapter
+//
+//        receiverName = intent.getStringExtra("name").toString()
+//        receiverUid= intent.getStringExtra("uId").toString()
+//
+//        mAuth = FirebaseAuth.getInstance()
+//        mDbRef = FirebaseDatabase.getInstance().reference
+//
+//        val senderUid = mAuth.currentUser?.uid
+//
+//        senderRoom = receiverUid + senderUid
+//        receiverRoom = senderUid + receiverUid
+//
+//        supportActionBar?.title=receiverName
+//
+//        binding.sendBtn.setOnClickListener {
+//
+//            val message = binding.messageEdit.text.toString()
+//            val messageObject = Message(message, senderUid)
+//
+//                //보낸이, 받는이 대화 내용 저장
+//            mDbRef.child("chats").child(senderRoom).child("messages").push()
+//                .setValue(messageObject).addOnSuccessListener {
+//                    mDbRef.child("chats").child(receiverRoom).child("messages").push()
+//                        .setValue(messageObject)
+//                }
+//            binding.messageEdit.setText("")
+//        }
+//
+//        mDbRef.child("chats").child(senderRoom).child("messages")
+//            .addValueEventListener(object:ValueEventListener{
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    messageList.clear()
+//
+//                    for (postSnapshat in snapshot.children){
+//
+//                        val message = postSnapshat.getValue(Message::class.java)
+//                        messageList.add(message!!)
+//                    }
+//                    messageAdapter.notifyDataSetChanged()
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {
+//
+//                }
+//            })
+//
+//    }
+//
+//
+//}
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var receiverName: String
     private lateinit var receiverUid: String
 
     private lateinit var binding: ActivityChatBinding
-
 
     lateinit var mAuth: FirebaseAuth
     lateinit var mDbRef: DatabaseReference
@@ -27,7 +103,6 @@ class ChatActivity : AppCompatActivity() {
 
     private lateinit var messageList:ArrayList<Message>
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityChatBinding.inflate(layoutInflater)
@@ -36,7 +111,9 @@ class ChatActivity : AppCompatActivity() {
         messageList = ArrayList()
         val messageAdapter : MessageAdapter = MessageAdapter(this, messageList)
 
-        binding.chatRecyclerView.layoutManager=LinearLayoutManager(this)
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.stackFromEnd = true // 최신 메시지가 보이도록 설정
+        binding.chatRecyclerView.layoutManager = layoutManager
         binding.chatRecyclerView.adapter = messageAdapter
 
         receiverName = intent.getStringExtra("name").toString()
@@ -53,17 +130,21 @@ class ChatActivity : AppCompatActivity() {
         supportActionBar?.title=receiverName
 
         binding.sendBtn.setOnClickListener {
-
             val message = binding.messageEdit.text.toString()
-            val messageObject = Message(message, senderUid)
+            if (message.isNotEmpty()) {
+                val messageObject = Message(message, senderUid)
 
-                //보낸이, 받는이 대화 내용 저장
-            mDbRef.child("chats").child(senderRoom).child("messages").push()
-                .setValue(messageObject).addOnSuccessListener {
-                    mDbRef.child("chats").child(receiverRoom).child("messages").push()
-                        .setValue(messageObject)
-                }
-            binding.messageEdit.setText("")
+                // 보낸이, 받는이 대화 내용 저장
+                mDbRef.child("chats").child(senderRoom).child("messages").push()
+                    .setValue(messageObject).addOnSuccessListener {
+                        mDbRef.child("chats").child(receiverRoom).child("messages").push()
+                            .setValue(messageObject)
+                    }
+                binding.messageEdit.setText("")
+
+                // 새 메시지가 추가될 때 최신 메시지로 스크롤
+                binding.chatRecyclerView.scrollToPosition(messageList.size - 1)
+            }
         }
 
         mDbRef.child("chats").child(senderRoom).child("messages")
@@ -71,18 +152,22 @@ class ChatActivity : AppCompatActivity() {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     messageList.clear()
 
-                    for (postSnapshat in snapshot.children){
-
-                        val message = postSnapshat.getValue(Message::class.java)
-                        messageList.add(message!!)
+                    for (postSnapshot in snapshot.children) {
+                        val message = postSnapshot.getValue(Message::class.java)
+                        if (message != null) {
+                            messageList.add(message)
+                        }
                     }
                     messageAdapter.notifyDataSetChanged()
+
+                    // 데이터 변경 시 최신 메시지로 스크롤
+                    binding.chatRecyclerView.scrollToPosition(messageList.size - 1)
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-
                 }
             })
-
     }
 }
+
+
